923636989b2598b1c7ee85a10d02267f860a5078
James O'Leary <jpohhhh@gmail.com>
2023-10-07 08:40:33 -0400

    Add model for miniLmL6V2 and method stub

--- lib/fonnx_method_channel.dart
+++ lib/fonnx_method_channel.dart
@@ -11,7 +11,16 @@ class MethodChannelFonnx extends FonnxPlatform {
 
   @override
   Future<String?> getPlatformVersion() async {
-    final version = await methodChannel.invokeMethod<String>('getPlatformVersion');
+    final version =
+        await methodChannel.invokeMethod<String>('getPlatformVersion');
     return version;
+  }
+
+  /// Create embeddings for [texts].
+  /// https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2
+  Future<List<List<double>>?> miniLmL6V2(List<String> texts) async {
+    final result = await methodChannel
+        .invokeMethod<List<List<double>>>('miniLmL6V2');
+    return result;
   }
 }
