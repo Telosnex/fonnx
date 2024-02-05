7d93964f8ced0f30f2d278245a87a658e825c35f
James O'Leary <65884233+jpohhhh@users.noreply.github.com>
2024-02-04 23:24:16 -0500

    Whisper Web trial

--- lib/models/whisper/whisper_web.dart
+++ lib/models/whisper/whisper_web.dart
@@ -1,8 +1,20 @@
-import 'dart:typed_data';
-
 import 'package:fonnx/models/whisper/whisper.dart';
+import 'package:js/js.dart';
+import 'package:js/js_util.dart';
 
 Whisper getWhisper(String path) => WhisperWeb(path);
+
+@JS()
+class Promise<T> {
+  external Promise(
+      void Function(void Function(T result) resolve, Function reject) executor);
+  external Promise then(void Function(T result) onFulfilled,
+      [Function onRejected]);
+}
+
+@JS('window.whisper')
+external Promise<List<List<double>>> whisperJs(
+    String modelPath, List<int> wordpieces);
 
 class WhisperWeb implements Whisper {
   @override
@@ -11,7 +23,15 @@ class WhisperWeb implements Whisper {
   WhisperWeb(this.modelPath);
 
   @override
-  Future<String> doInference(Uint8List bytes) {
-    throw UnimplementedError();
+  Future<String> doInference(List<int> audioBytes) async {
+    final jsObject = await promiseToFuture(whisperJs(modelPath, audioBytes));
+
+    if (jsObject == null) {
+      throw Exception('Inference returned from JS code is null');
+    }
+
+    final jsList = (jsObject as List<dynamic>);
+    final result = jsList.cast<String>().join(' ');
+    return result;
   }
 }
