71aa5f7ed8904b8cf3b4cf0a2469ac199e7bf248
James O'Leary <65884233+jpohhhh@users.noreply.github.com>
2024-06-12 11:40:56 -0400

    Demo WAV generated respects voice frames ignored; extract big button handle

--- example/lib/tts_demo_widget.dart
+++ example/lib/tts_demo_widget.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'dart:math' as math;
 
 import 'package:audioplayers/audioplayers.dart';
+import 'package:collection/collection.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -24,6 +25,7 @@ class _TtsDemoWidgetState extends State<TtsDemoWidget> {
   SttServiceResponse? _sttServiceResponse;
   StreamSubscription? _sttStreamSubscription;
   SttService? _sttService;
+  AudioPlayer? _audioPlayer;
   var _sttIsVoiceThreshold = SttService.kVadPIsVoiceThreshold;
 
   @override
@@ -63,7 +65,7 @@ class _TtsDemoWidgetState extends State<TtsDemoWidget> {
         ),
         if (_sttServiceResponse != null) ...[
           heightPadding,
-          Text(
+          SelectableText(
             _sttServiceResponse!.transcription,
             style: Theme.of(context).textTheme.bodyMedium,
           ),
@@ -73,8 +75,11 @@ class _TtsDemoWidgetState extends State<TtsDemoWidget> {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.end,
-              children: _sttServiceResponse!.audioFrames.map((e) {
-                final index = _sttServiceResponse!.audioFrames.indexOf(e);
+              children: _sttServiceResponse!.audioFrames.reversed
+                  .take(20)
+                  .toList()
+                  .reversed
+                  .mapIndexed((index, e) {
                 final Color color;
                 if (e?.vadP == null) {
                   color = Colors.transparent;
@@ -128,152 +133,150 @@ class _TtsDemoWidgetState extends State<TtsDemoWidget> {
           ElevatedButton.icon(
             icon: const Icon(Icons.play_arrow),
             label: const Text("Play"),
-            onPressed: () async {
-              final frames = _sttServiceResponse!.audioFrames;
-              int? firstFrameIndex;
-              int? lastFrameIndex;
-              for (var i = 0; i < frames.length; i++) {
-                final frame = frames[i];
-                if (frame?.vadP == null) {
-                  // Audio hasn't been processed.
-                  continue; // Continues to next iteration if audio hasn't been processed, instead of breaking.
-                }
-                // Check for the first non-silent frame
-                if (firstFrameIndex == null &&
-                    frame!.vadP! > _sttIsVoiceThreshold) {
-                  firstFrameIndex = i;
-                }
-                // Update the last non-silent frame index whenever a non-silent frame is encountered
-                if (frame!.vadP! > _sttIsVoiceThreshold) {
-                  lastFrameIndex = i;
-                }
-              }
-// Return or perform further actions only if both first and last non-silent frames are found
-              if (firstFrameIndex == null || lastFrameIndex == null) {
-                return;
-              }
-              final framesToProcess = frames.sublist(
-                math.max(firstFrameIndex - 3, 0),
-                math.min(lastFrameIndex + 10, frames.length),
-              );
-              debugPrint(
-                  'Detected ${framesToProcess.length} frames of voice (from ${frames.whereType<AudioFrame>().toList().length} of audio @ threshold $_sttIsVoiceThreshold)');
-              final indexOfFirstSpeech = frames.indexWhere((frame) {
-                return frame?.vadP != null &&
-                    frame!.vadP! >= _sttIsVoiceThreshold;
-              });
-              // Intent: capture ~100ms of audio before the first speech.
-              final startIndex = math.max(0, indexOfFirstSpeech - 3);
-              final voiceFrames = frames.sublist(startIndex);
-
-              Uint8List generateWavHeader(
-                int pcmDataLength, {
-                required int bitsPerSample,
-                required int numChannels,
-                required int sampleRate,
-              }) {
-                int fileSize = pcmDataLength +
-                    44 -
-                    8; // Add WAV header size except for 'RIFF' and its size field
-                int byteRate = sampleRate * numChannels * bitsPerSample ~/ 8;
-                int blockAlign = numChannels * bitsPerSample ~/ 8;
-
-                var header = Uint8List(44);
-                var buffer = ByteData.view(header.buffer);
-
-                // RIFF header
-                buffer.setUint32(0, 0x52494646, Endian.big); // 'RIFF'
-                buffer.setUint32(4, fileSize, Endian.little);
-                buffer.setUint32(8, 0x57415645, Endian.big); // 'WAVE'
-
-                // fmt subchunk
-                buffer.setUint32(12, 0x666d7420, Endian.big); // 'fmt '
-                buffer.setUint32(
-                    16, 16, Endian.little); // Subchunk1 size (16 for PCM)
-                buffer.setUint16(
-                    20, 1, Endian.little); // Audio format (1 for PCM)
-                buffer.setUint16(
-                    22, numChannels, Endian.little); // Number of channels
-                buffer.setUint32(24, sampleRate, Endian.little); // Sample rate
-                buffer.setUint32(28, byteRate, Endian.little); // Byte rate
-                buffer.setUint16(32, blockAlign, Endian.little); // Block align
-                buffer.setUint16(
-                    34, bitsPerSample, Endian.little); // Bits per sample
-
-                // data subchunk
-                buffer.setUint32(36, 0x64617461, Endian.big); // 'data'
-                buffer.setUint32(40, pcmDataLength,
-                    Endian.little); // Subchunk2 size (PCM data size)
-
-                return header;
-              }
-
-              Uint8List generateWavFile(
-                List<int> pcmData, {
-                required int bitsPerSample,
-                required int numChannels,
-                required int sampleRate,
-              }) {
-                final header = generateWavHeader(
-                  pcmData.length,
-                  sampleRate: sampleRate,
-                  numChannels: numChannels,
-                  bitsPerSample: bitsPerSample,
-                );
-                final wavFile = Uint8List(header.length + pcmData.length);
-                wavFile.setAll(0, header);
-                wavFile.setAll(header.length, pcmData);
-                return wavFile;
-              }
-
-              /// Returns null if no frames are above the threshold.
-              Uint8List? wavFromFrames(
-                  {required List<AudioFrame> frames, required double minVadP}) {
-                final bytes = frames
-                    .where((e) => e.vadP != null && e.vadP! >= minVadP)
-                    .map((e) => e.bytes)
-                    .expand((element) => element);
-                if (bytes.isEmpty) {
-                  return null;
-                }
-                final bytesList = bytes.toList();
-                return generateWavFile(
-                  bytesList,
-                  bitsPerSample: SttService.kBitsPerSample,
-                  numChannels: SttService.kChannels,
-                  sampleRate: SttService.kSampleRate,
-                );
-              }
-
-              final playWav = wavFromFrames(
-                frames: voiceFrames.nonNulls.toList(),
-                minVadP: 0,
-              );
-              if (playWav == null) {
-                debugPrint('No frames with voice, skipping WAV creation.');
-                return;
-              }
-              if (kIsWeb) {
-                String base64String = base64Encode(playWav);
-
-                // Step 4: Create the data URL
-                final url = 'data:audio/wav;base64,$base64String';
-                final player = AudioPlayer();
-                player.play(UrlSource(url));
-              } else {
-                final tempDir = await path_provider.getTemporaryDirectory();
-                final playWavPath = path.join(tempDir.path, 'voice.wav');
-                final playWavFile = File(playWavPath);
-                await playWavFile.writeAsBytes(playWav);
-                debugPrint('Wrote voice to $playWavPath');
-                final player = AudioPlayer();
-                player.play(DeviceFileSource(playWavPath));
-              }
-            },
+            onPressed: _playButtonPressed,
           ),
         ]
       ],
     );
+  }
+
+  void _playButtonPressed() async {
+    final frames = _sttServiceResponse!.audioFrames;
+    int? firstFrameIndex;
+    int? lastFrameIndex;
+    for (var i = 0; i < frames.length; i++) {
+      final frame = frames[i];
+      if (frame?.vadP == null) {
+        // Audio hasn't been processed.
+        continue; // Continues to next iteration if audio hasn't been processed, instead of breaking.
+      }
+      // Check for the first non-silent frame
+      if (firstFrameIndex == null && frame!.vadP! > _sttIsVoiceThreshold) {
+        firstFrameIndex = i;
+      }
+      // Update the last non-silent frame index whenever a non-silent frame is encountered
+      if (frame!.vadP! > _sttIsVoiceThreshold) {
+        lastFrameIndex = i;
+      }
+    }
+// Return or perform further actions only if both first and last non-silent frames are found
+    if (firstFrameIndex == null || lastFrameIndex == null) {
+      return;
+    }
+    final framesToProcess = frames.sublist(
+      math.max(firstFrameIndex - 3, 0),
+      math.min(lastFrameIndex + 10, frames.length),
+    );
+    debugPrint(
+        'Detected ${framesToProcess.length} frames of voice (from ${frames.whereType<AudioFrame>().toList().length} of audio @ threshold $_sttIsVoiceThreshold)');
+    final indexOfFirstSpeech = frames.indexWhere((frame) {
+      return frame?.vadP != null && frame!.vadP! >= _sttIsVoiceThreshold;
+    });
+    // Intent: capture ~100ms of audio before the first speech.
+    final startIndex = math.max(0, indexOfFirstSpeech - 3);
+    final voiceFrames = frames
+        .sublist(startIndex)
+        .where((e) => e?.vadP != null && e!.vadP! > _sttIsVoiceThreshold);
+
+    Uint8List generateWavHeader(
+      int pcmDataLength, {
+      required int bitsPerSample,
+      required int numChannels,
+      required int sampleRate,
+    }) {
+      int fileSize = pcmDataLength +
+          44 -
+          8; // Add WAV header size except for 'RIFF' and its size field
+      int byteRate = sampleRate * numChannels * bitsPerSample ~/ 8;
+      int blockAlign = numChannels * bitsPerSample ~/ 8;
+
+      var header = Uint8List(44);
+      var buffer = ByteData.view(header.buffer);
+
+      // RIFF header
+      buffer.setUint32(0, 0x52494646, Endian.big); // 'RIFF'
+      buffer.setUint32(4, fileSize, Endian.little);
+      buffer.setUint32(8, 0x57415645, Endian.big); // 'WAVE'
+
+      // fmt subchunk
+      buffer.setUint32(12, 0x666d7420, Endian.big); // 'fmt '
+      buffer.setUint32(16, 16, Endian.little); // Subchunk1 size (16 for PCM)
+      buffer.setUint16(20, 1, Endian.little); // Audio format (1 for PCM)
+      buffer.setUint16(22, numChannels, Endian.little); // Number of channels
+      buffer.setUint32(24, sampleRate, Endian.little); // Sample rate
+      buffer.setUint32(28, byteRate, Endian.little); // Byte rate
+      buffer.setUint16(32, blockAlign, Endian.little); // Block align
+      buffer.setUint16(34, bitsPerSample, Endian.little); // Bits per sample
+
+      // data subchunk
+      buffer.setUint32(36, 0x64617461, Endian.big); // 'data'
+      buffer.setUint32(
+          40, pcmDataLength, Endian.little); // Subchunk2 size (PCM data size)
+
+      return header;
+    }
+
+    Uint8List generateWavFile(
+      List<int> pcmData, {
+      required int bitsPerSample,
+      required int numChannels,
+      required int sampleRate,
+    }) {
+      final header = generateWavHeader(
+        pcmData.length,
+        sampleRate: sampleRate,
+        numChannels: numChannels,
+        bitsPerSample: bitsPerSample,
+      );
+      final wavFile = Uint8List(header.length + pcmData.length);
+      wavFile.setAll(0, header);
+      wavFile.setAll(header.length, pcmData);
+      return wavFile;
+    }
+
+    /// Returns null if no frames are above the threshold.
+    Uint8List? wavFromFrames(
+        {required List<AudioFrame> frames, required double minVadP}) {
+      final bytes = frames
+          .where((e) => e.vadP != null && e.vadP! >= minVadP)
+          .map((e) => e.bytes)
+          .expand((element) => element);
+      if (bytes.isEmpty) {
+        return null;
+      }
+      final bytesList = bytes.toList();
+      return generateWavFile(
+        bytesList,
+        bitsPerSample: SttService.kBitsPerSample,
+        numChannels: SttService.kChannels,
+        sampleRate: SttService.kSampleRate,
+      );
+    }
+
+    final playWav = wavFromFrames(
+      frames: voiceFrames.nonNulls.toList(),
+      minVadP: 0,
+    );
+    if (playWav == null) {
+      debugPrint('No frames with voice, skipping WAV creation.');
+      return;
+    }
+    if (kIsWeb) {
+      String base64String = base64Encode(playWav);
+
+      // Step 4: Create the data URL
+      final url = 'data:audio/wav;base64,$base64String';
+      _audioPlayer ??= AudioPlayer();
+      _audioPlayer!.play(UrlSource(url));
+    } else {
+      final tempDir = await path_provider.getTemporaryDirectory();
+      final playWavPath = path.join(tempDir.path, 'voice.wav');
+      final playWavFile = File(playWavPath);
+      await playWavFile.writeAsBytes(playWav);
+      debugPrint('Wrote voice to $playWavPath');
+      _audioPlayer ??= AudioPlayer();
+      _audioPlayer!.play(DeviceFileSource(playWavPath));
+    }
   }
 
   void _runVadDemo() async {
@@ -287,9 +290,13 @@ class _TtsDemoWidgetState extends State<TtsDemoWidget> {
       return;
     }
     final vadModelPath = await getModelPath('silero_vad.onnx');
-    final whisperModelPath = await getWhisperModelPath('whisper_tiny.onnx');
+    final whisperModelPath = await getWhisperModelPath('whisper_small.onnx');
     final service = SttService(
-        vadModelPath: vadModelPath, whisperModelPath: whisperModelPath);
+      vadModelPath: vadModelPath,
+      whisperModelPath: whisperModelPath,
+      voiceThreshold: _sttIsVoiceThreshold,
+      maxDuration: const Duration(seconds: 10),
+    );
     _sttService = service;
     final subscription = service.transcribe().listen((event) {
       setState(() {
