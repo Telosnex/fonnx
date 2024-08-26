b36bc2486d7baf9abcd61085aa5fa67f11bb8e23
James O'Leary <65884233+jpohhhh@users.noreply.github.com>
2024-08-26 15:35:17 -0400

    WASM: fix Silero getting int & double arrays

--- lib/models/sileroVad/silero_vad_web.dart
+++ lib/models/sileroVad/silero_vad_web.dart
@@ -7,8 +7,8 @@ import 'package:fonnx/models/sileroVad/silero_vad.dart';
 SileroVad getSileroVad(String path) => SileroVadWeb(path);
 
 @JS('window.sileroVad')
-external JSPromise<JSString?> sileroVadJs(String modelPath, JSUint8Array audioBytes, String previousStateAsJsonString);
-
+external JSPromise<JSString?> sileroVadJs(String modelPath,
+    JSUint8Array audioBytes, String previousStateAsJsonString);
 
 class SileroVadWeb implements SileroVad {
   @override
@@ -20,7 +20,9 @@ class SileroVadWeb implements SileroVad {
   Future<Map<String, dynamic>> doInference(Uint8List bytes,
       {Map<String, dynamic> previousState = const {}}) async {
     final previousStateAsJsonString = json.encode(previousState);
-    final jsObject = await sileroVadJs(modelPath, bytes.toJS, previousStateAsJsonString).toDart;
+    final jsObject =
+        await sileroVadJs(modelPath, bytes.toJS, previousStateAsJsonString)
+            .toDart;
 
     if (jsObject == null) {
       throw Exception('Silero VAD result returned from JS code is null');
@@ -32,15 +34,27 @@ class SileroVadWeb implements SileroVad {
     }
 
     final recasted = <String, dynamic>{};
-    if (dartObject.containsKey('cn')) {
-      recasted['cn'] = Float32List.fromList(dartObject['cn'].cast<double>());
-    }
-    if (dartObject.containsKey('hn')) {
-      recasted['hn'] = Float32List.fromList(dartObject['hn'].cast<double>());
-    }
-    if (dartObject.containsKey('output')) {
-      recasted['output'] =
-          Float32List.fromList(dartObject['output'].cast<double>());
+    final keysToRecast = ['cn', 'hn', 'output'];
+
+    for (final key in keysToRecast) {
+      if (!dartObject.containsKey(key)) {
+        continue;
+      }
+
+      final List<dynamic> array = dartObject[key];
+      if (array.isEmpty) {
+        continue;
+      }
+
+      // Convert all elements to double, handling both int and double
+      // - We used to be able to assume that the JS was List<double>
+      // - WASM x Flutter Web introduced an issue - decoded JSON has a List
+      //   that are both int and double. This seems to happen on louder input,
+      //   my guess is 1.0 is being converted to 1 in JS.
+      // - Without this recasting, Dart code crashes in WASM.
+      final List<double> doubleArray =
+          array.map((e) => e is int ? e.toDouble() : (e as double)).toList();
+      recasted[key] = Float32List.fromList(doubleArray);
     }
     return recasted;
   }
