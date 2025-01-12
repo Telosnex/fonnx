f4af3f664bb9146b86a1687dfc755d37e71be4cf
James O'Leary <65884233+jpohhhh@users.noreply.github.com>
2025-01-12 13:51:18 -0500

    fix: cosine similarity throws when vectors unequal lengths

--- lib/extensions/vector.dart
+++ lib/extensions/vector.dart
@@ -3,6 +3,10 @@ import 'package:ml_linalg/vector.dart';
 
 extension Similarity on Vector {
   double cosineSimilarity(Vector vector) {
+    if (length != vector.length) {
+      print('Fonnx.Vector.Similarity.cosineSimilarity: Vectors must have the same length. A vector has length $length, while the other has length ${vector.length}. Returning 0 for similarity.');
+      return 0.0;
+    }
     final distance = distanceTo(vector, distance: Distance.cosine);
     return 1.0 - distance;
   }
